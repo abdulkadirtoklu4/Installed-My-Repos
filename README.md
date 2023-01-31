ef8d8f9cc6ac35f056dc9105a1bedd14a9b91ee1
abdulkadirtoklu4 <abdulkadir74280@gmail.com>
2023-01-31 16:23:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installed-My-Repos
+
+## Hello Linux Users!
+
+The number of my warehouses is increasing, so I decided to write a new automation system.
+Now you will be able to download many tools at once with this automation system.
---
+++ myRepos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/kali/MasaÃ¼stÃ¼/
+mkdir "abdulkadirtoklu4's Repos"
+cd "abdulkadirtoklu4's Repos"
+git clone https://github.com/abdulkadirtoklu4/Linux-Automation-System
+git clone https://github.com/abdulkadirtoklu4/Web-Site
+git clone https://github.com/abdulkadirtoklu4/CsCalculator
+git clone https://github.com/abdulkadirtoklu4/Python-Calculator
+git clone https://github.com/abdulkadirtoklu4/Superman-Logo
+clear
+echo -e "\033[32m[+]\033[0m Installed 'Linux-Automation-System' Repo."
+echo -e "\033[32m[+]\033[0m Installed 'Web-Site' Repo."
+echo -e "\033[32m[+]\033[0m Installed 'CsCalculator' Repo."
+echo -e "\033[32m[+]\033[0m Installed 'Python-Calculator' Repo."
+echo -e "\033[32m[+]\033[0m Installed 'Superman-Logo' Repo."

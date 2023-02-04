3adda7c5164810461e2c8269c0c08539c370bbc9
abdulkadirtoklu4 <abdulkadir74280@gmail.com>
2023-02-04 23:28:39 +0300

    1 Repo Added.

--- myRepos.sh
+++ myRepos.sh
@@ -8,9 +8,11 @@ git clone https://github.com/abdulkadirtoklu4/Web-Site
 git clone https://github.com/abdulkadirtoklu4/CsCalculator
 git clone https://github.com/abdulkadirtoklu4/Python-Calculator
 git clone https://github.com/abdulkadirtoklu4/Superman-Logo
+git clone https://github.com/abdulkadirtoklu4/Python-QRCode-Converter
 clear
 echo -e "\033[32m[+]\033[0m Installed 'Linux-Automation-System' Repo."
 echo -e "\033[32m[+]\033[0m Installed 'Web-Site' Repo."
 echo -e "\033[32m[+]\033[0m Installed 'CsCalculator' Repo."
 echo -e "\033[32m[+]\033[0m Installed 'Python-Calculator' Repo."
 echo -e "\033[32m[+]\033[0m Installed 'Superman-Logo' Repo."
+echo -e "\033[32m[+]\033[0m Installed 'Python-QRCode-Converter' Repo."
